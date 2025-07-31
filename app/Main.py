e0d3d2c4518b0be9c6c01dd9242276b7270091a5
Bruno Belarmino da Silva <brunobelarmino336@outlook.com>
2025-07-31 17:46:48 -0300

    Instanciando a classe Conta

--- app/Main.py
+++ app/Main.py
@@ -8,6 +8,9 @@ from Cliente import Cliente
 from Conta import Conta
 
 c1 = Cliente("Bruno", "(81) 99999-9999")
+conta = Conta(c1.nome, 6565, 0)
 
-print(c1)
-print(c1.nome, "e", c1.telefone)
+# print(c1)
+# print(c1.nome, "e", c1.telefone)
+
+print(conta.titular, " Numero: ", conta.numero, "Seu Saldo: ", conta.saldo)
