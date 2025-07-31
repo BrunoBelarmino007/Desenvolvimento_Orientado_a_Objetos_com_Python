57ff229024d45068a7eccf51df54185550aaf935
Bruno Belarmino da Silva <brunobelarmino336@outlook.com>
2025-07-31 19:56:13 -0300

    Encapsulando: adicionando os metodos de acesso (Get e Set) na classe Conta

--- app/Conta.py
+++ app/Conta.py
@@ -4,3 +4,13 @@ class Conta:
         self.numero = numero
         self.titular = titular
         
+    # Método Get
+    def get_saldo(self):
+        return self._saldo
+    
+    # Método Set
+    def set_saldo(self, valor):
+        if (valor < 0):
+            print("O saldo não pode ser negativo")
+        else:
+            self._saldo = valor
