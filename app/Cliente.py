bd9e07ebea4160023680b9931078a6a68cdd6a7b
Bruno Belarmino da Silva <brunobelarmino336@outlook.com>
2025-07-31 19:51:21 -0300

    Encapsulando: adicionando os metodos de acesso (Get e Set) na classe cliente

--- app/Cliente.py
+++ app/Cliente.py
@@ -4,3 +4,10 @@ class Cliente:
         self._nome = n
         self._telefone = fone
         
+    # Método Get
+    def get_nome(self):
+        return self._nome
+    
+    # Método Set
+    def set_nome(self, nome):  
+        self._nome = nome
